336a0c1282b1d83cff2c9e4ae07eec29f125e114
Lemonade-up <wmaglangit1118@gmail.com>
2021-10-23 20:27:25 +0800

    Fixed Integer and Input Function

--- Program no.2.py
+++ Program no.2.py
@@ -1,5 +1,5 @@
-amountApple = input('How many Apples do you want to buy?')
-amountOrange = input('How many Oranges do you want to buy?')
+amountApple = int(input('How many Apples do you want to buy?'))
+amountOrange = int(input('How many Oranges do you want to buy?'))
 priceApple = 20
 priceOrange = 25
 priceTotal = amountApple * priceApple + amountOrange * priceOrange
