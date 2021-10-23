b259832eb2d101f5e2bbf4a06d855d42730a651b
Lemonade-up <wmaglangit1118@gmail.com>
2021-10-23 20:26:41 +0800

    Formula added

--- Program no.2.py
+++ Program no.2.py
@@ -1,2 +1,5 @@
 amountApple = input('How many Apples do you want to buy?')
 amountOrange = input('How many Oranges do you want to buy?')
+priceApple = 20
+priceOrange = 25
+priceTotal = amountApple * priceApple + amountOrange * priceOrange
