7e35ec7b30ea8acd0ff1666c07e3fb0ef4c01874
Lemonade-up <wmaglangit1118@gmail.com>
2021-10-24 14:43:58 +0800

    Formula Added

--- Program no.3.py
+++ Program no.3.py
@@ -1,2 +1,5 @@
 totalCash = int(input("How much money do you have?: "))
 priceApple = int(input("How much is an Apple?: "))
+
+amountApple = totalCash // priceApple
+changeCash = totalCash % priceApple
